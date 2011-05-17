f8f262940e0aca245f8630d7c5c113ae6db06fbc
Xavier Lange <xrlange@gmail.com>
2011-05-16 23:31:08 -0700

    Args test

---
+++ test/args_test.rb
@@ -0,0 +1,27 @@
+require "test/unit"
+require './test/helper_methods'
+
+class ArgsTest < Test::Unit::TestCase
+  include TestHelperMethods
+
+  def test_parse
+    assert_nothing_raised do
+      parse "()", :args
+    end
+    assert_nothing_raised do
+      parse "(nil)", :args
+    end
+    assert_nothing_raised do
+      parse "(nil,nil)", :args
+    end
+    assert_nothing_raised do
+      parse "(nil, nil )", :args
+    end
+  end
+
+  def test_bad
+    assert_raise RuntimeError do
+      parse "(nil,)", :args
+    end
+  end
+end
