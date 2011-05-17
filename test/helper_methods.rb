7d8e1e43dbc6879d336c5bbd20442c0be01c93e6
Xavier Lange <xrlange@gmail.com>
2011-05-16 21:38:28 -0700

    Parse helper now checks that entire string is consumed

--- test/helper_methods.rb
+++ test/helper_methods.rb
@@ -3,6 +3,9 @@ require 'lupin'
 module TestHelperMethods
   def parse (input, rule)
     parser = Lupin::Parser.new(input)
+    # Is the string at least partially acceptable?
     raise parser.failure_info unless parser.parse(rule.to_s)
+    # Is the string entirely acceptable?
+    raise parser.failure_info unless parser.pos == input.length
   end
 end
