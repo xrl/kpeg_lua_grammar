7a8c69067f0485460c66e81c2dbedbef438edd65
Xavier Lange <xrlange@gmail.com>
2011-05-25 00:04:42 -0700

    [TEST] String passing

--- test/string_test.rb
+++ test/string_test.rb
@@ -6,40 +6,40 @@ class StringTest < Test::Unit::TestCase
   
   def test_strings
     assert_nothing_raised do
-      parse "'foobar'", :string
-      parse "'foo\\'bar'", :string
-      parse "'foo\\\nbar'", :string
-      parse "'foo\"bar'", :string
+      parse :string, %Q{'foobar'}, [:string, "foobar"]
+      parse :string, %Q{'foo\\'bar'}, [:string, %Q{foo\\'bar}]
+      parse :string, %Q{'foo\\\nbar'}, [:string, %Q{foo\\\nbar}]
+      parse :string, %Q{'foo"bar'}, [:string, %Q{foo\"bar}]
     end
     
     assert_nothing_raised do
-      parse "\"foobar\"", :string
-      parse "\"foo\\\"bar\"", :string
-      parse "\"foo\\\nbar\"", :string
-      parse "\"foo'bar\"", :string
+      parse :string, %Q{"foobar"}, [:string,%Q{foobar}]
+      parse :string, %Q{"foo\\\"bar"}, [:string,%Q{foo\\\"bar}]
+      parse :string, %Q{"foo\\\nbar"}, [:string,%Q{foo\\\nbar}]
+      parse :string, %Q{"foo'bar"}, [:string,%Q{foo'bar}]
     end
     
     assert_nothing_raised do
-      parse "[[foo]]", :string
-      parse "[=[foo]]bar]=]", :string
-      parse "[[foo'bar\"\nbaz]]", :string
+      parse :string, "[[foo]]", [:string,%Q{foo}]
+      parse :string, "[=[foo]]bar]=]", [:string,%Q{foo]]bar}]
+      parse :string, "[[foo'bar\"\nbaz]]", [:string,%Q{foo'bar\"\nbaz}]
     end
   end
   
   def test_bad_strings
-    assert_raise RuntimeError do
-      parse "'foo bar", :string
-      parse "'foo\nbar'", :string
+    assert_raise Test::Unit::AssertionFailedError do
+      parse :string, "'foo bar", [:string, %Q{'foo bar}]
+      parse :string, "'foo\nbar'", [:string, %Q{'foo\nbar'}]
     end
     
-    assert_raise RuntimeError do
-      parse "\"foo bar", :string
-      parse "\"foo\nbar\"", :string
+    assert_raise Test::Unit::AssertionFailedError do
+      parse :string, "\"foo bar", [:string, %Q{}]
+      parse :string, "\"foo\nbar\"", [:string,%Q{}]
     end
     
-    assert_raise RuntimeError do
-      parse "[[foo bar", :string
-      parse "[=[foo]]", :string
+    assert_raise Test::Unit::AssertionFailedError do
+      parse :string, "[[foo bar", [:string, %Q{}]
+      parse :string, "[=[foo]]", [:string, %Q{}]
     end
   end
 end
