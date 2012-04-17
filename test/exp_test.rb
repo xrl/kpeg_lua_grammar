28455499d0dd43f23866b011823c64680604eac8
Kris Barrett <krisbarrett@gmail.com>
2012-04-16 20:16:02 -0700

    Added functioncall back to exp

--- test/exp_test.rb
+++ test/exp_test.rb
@@ -9,27 +9,20 @@ class ExpTest < MiniTest::Unit::TestCase
     parse :exp, "1", [:number, 1.0]
     parse :exp, %Q{"kitties"}, [:string, "kitties"]
     parse :exp, "k()", [:functioncall, [:name,:k], []]
-    # parse :exp, "(kitties())", [:functioncall, :kitties, []]
-    # parse :exp, "(((kitties()))", [:functioncall, :kitties, []]
+    parse :exp, "(kitties())", [:functioncall, [:name, :kitties], []]
+    parse :exp, "((kitties()))", [:functioncall, [:name, :kitties], []]
     # parse :exp, "kitties(puppies(iguanas()))"
-    # p = Lupin::Parser.new("1+1")
-    # assert p.parse('exp')
-    # assert p.result, [:+, 1, 1]
-    # parse "(1+1)", :exp
-    # parse "1-1.0", :exp
-    # parse "1/10", :exp
-    # parse "1*100e100", :exp
-    # parse "...", :exp # How is this used? :-D
-    # parse "puppies(1+1,\"woof\")", :exp
-    # parse "some_var", :exp
-    # parse "(funky_func(1+1))", :exp
-    # parse "+++"
-  end
-
-  def test_bad
-    # no_parse :exp, "-"
-    # no_parse :exp, "*"
-    # # Excessive? :-D
-    # no_parse :exp, "((((((((((((((((kitties())))))))))))))))"
+    #    p = Lupin::Parser.new("1+1")
+    #    assert p.parse('exp')
+    #    assert p.result, [:+, 1, 1]
+    #    parse "(1+1)", :exp
+    #    parse "1-1.0", :exp
+    #    parse "1/10", :exp
+    #    parse "1*100e100", :exp
+    #    parse "...", :exp # How is this used? :-D
+    #    parse "puppies(1+1,\"woof\")", :exp
+    #    parse "some_var", :exp
+    #    parse "(funky_func(1+1))", :exp
+    #    parse "+++"
   end
 end
